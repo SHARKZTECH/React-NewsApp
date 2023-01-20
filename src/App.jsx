2d9a43dfacf7682655b9c2f21c758fb879f77916
sharkz <mosenjemudogoz@gmail.com>
2023-01-20 13:10:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react'
+import { Card, Container ,Col,Row, Nav, Navbar} from 'react-bootstrap';
+import './App.css'
+import {NEWS} from "./news";
+import LOGO from "./assets/logo.png"
+
+function App() {
+  const [category,setCategory]=useState("business");
+  const [news,setNews]=useState([]);
+
+  const getNews=async()=>{
+    const res=await fetch(`https://newsdata.io/api/1/news?apikey=pub_1586642de0b2bce5a7b07f566bc69ce9cb7f5&country=ke&category=${category}`);
+     const {results}=await res.json();
+     setNews(results);
+  }
+
+    // getNews();
+
+    useEffect(()=>{setNews(NEWS)},[category])
+
+
+
+  return (
+    <div className='body'>
+      <Navbar bg='light' expand="lg" className='mb-3'>
+        <Container>
+          <Navbar.Brand>
+            <img src={LOGO} alt="logo" 
+            style={{width:"4rem",height:"3rem"}}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className='me-auto'>
+              <Nav.Link
+              onClick={()=>setCategory("business")}>Business</Nav.Link>
+              <Nav.Link
+               onClick={()=>setCategory("entertainment")}>Entertainment</Nav.Link>
+              <Nav.Link
+               onClick={()=>setCategory("environment")}>Environment</Nav.Link>
+              <Nav.Link
+               onClick={()=>setCategory("politics")}>Politics</Nav.Link>
+              <Nav.Link
+               onClick={()=>setCategory("sports")}>Sports</Nav.Link>
+              <Nav.Link
+               onClick={()=>setCategory("top")}>Top</Nav.Link>
+              <Nav.Link
+               onClick={()=>setCategory("world")}>Wolrd</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>  
+      <Row  xs={1} md={2} className="g-4">
+      {news.map((n)=>(
+            <Col>
+            <Card style={{height:"20rem"}}>           
+               <Card.Body>
+                <Card.Title>
+                  {n.title}
+                </Card.Title>
+                <Card.Text>
+                  <p className='text_'>{n.description}</p>
+                  <div className='text-muted'>
+                  <small>Updated: {n.pubDate}</small>
+                  <br></br>
+                  <small>Source: {n.source_id}</small>
+                  </div>
+                  <p>
+                    <a href={n.link}>Read More...</a>
+                  </p>
+                </Card.Text>
+               </Card.Body>
+            </Card>
+          </Col>
+      ))}    
+      </Row>
+    </div>
+  )
+}
+
+export default App
---
+++ src/news.js
@@ -0,0 +1,210 @@
+export const NEWS=[
+    {
+      "title": "Horizon Forbidden West – Új előzetesen a nyugati vidékek kihívásai",
+      "link": "https://www.pcguru.hu/hirek/horizon-forbidden-west-uj-elozetesen-a-nyugati-videkek-kihivasai/69101",
+      "keywords": null,
+      "creator": null,
+      "video_url": null,
+      "description": "Aloy kalandjának folytatásában úgy tűnik nem sok alkalmunk lesz tétlenül, malmozva ücsörögni.",
+      "content": null,
+      "pubDate": "2022-02-04 07:07:01",
+      "image_url": "http://www.pcguru.hu/uploads/news/mid/horizon-forbidden-west-uj-elozetesen-a-nyugati-videkek-kihivasai-hirek-4acebfe659a8326cb803-mid.jpg",
+      "source_id": "pcguru",
+      "country": [
+      "hungary"
+        ],
+        "category": [
+        "entertainment"
+        ],
+        "language": "hungarian"
+   },
+      {
+      "title": "Wiceprezes Kruka sprzedał akcje za 4,1 mln zł",
+      "link": "https://www.pb.pl/wiceprezes-kruka-sprzedal-akcje-za-41-mln-zl-1140391",
+      "keywords": null,
+      "creator": null,
+      "video_url": null,
+      "description": "Michał Zasępa, wiceprezes Kruka, sprzedał na sesjach od 31 stycznia do 3 lutego akcje warte 4,1 mln zł.",
+      "content": null,
+      "pubDate": "2022-02-04 07:06:55",
+      "image_url": null,
+      "source_id": "pb",
+      "country": [
+      "poland"
+      ],
+      "category": [
+      "business"
+      ],
+      "language": "polish"
+      },
+      {
+      "title": "Byli szefowie Satisu wezwali do zrzeczenia się roszczeń",
+      "link": "https://www.pb.pl/byli-szefowie-satisu-wezwali-do-zrzeczenia-sie-roszczen-1140390",
+      "keywords": null,
+      "creator": null,
+      "video_url": null,
+      "description": "Grzegorz Kiczmachowski i Szymon Pura, byli menedżerowie Satis Group, przesłali do spółki wezwanie do zrzeczenia się roszczeń.",
+      "content": null,
+      "pubDate": "2022-02-04 07:06:55",
+      "image_url": null,
+      "source_id": "pb",
+      "country": [
+      "poland"
+      ],
+      "category": [
+      "business"
+      ],
+      "language": "polish"
+      },
+        {
+        "title": "PSV komt na het ‘nein’ van Schmidt met een lokroep, pakt de Teletijdmachine of gaat weer iets nieuws doen",
+        "link": "https://www.ed.nl/psv/psv-komt-na-het-nein-van-schmidt-met-een-lokroep-pakt-de-teletijdmachine-of-gaat-weer-iets-nieuws-doen~afe4286c/",
+        "keywords": null,
+        "creator": [
+        "Rik Elfrink"
+        ],
+        "video_url": null,
+        "description": "Kort nadat de spelersgroep van PSV gistermorgen door trainer Roger Schmidt zelf was ingelicht, drukte de persafdeling van de Eindhovense club op alle beschikbare knoppen die iets aan de buitenwereld verzenden. De coach verlengt zijn aflopende contract niet en dus heeft PSV alle tijd om voor volgend seizoen een opvolger aan te trekken. Wie moet dat worden?",
+        "content": null,
+        "pubDate": "2022-02-04 07:05:20",
+        "image_url": null,
+        "source_id": "ed",
+        "country": [
+        "netherland"
+        ],
+        "category": [
+        "sports"
+        ],
+        "language": "dutch"
+        },
+        {
+        "title": "Megerősítve a Ghostwire: Tokyo márciusi megjelenése, friss látnivalókat is kaptunk a játékból",
+        "link": "http://www.gamechannel.hu/cikk/hirblock/megerositve-a-ghostwire-tokyo-marciusi-megjelenese-friss-latnivalokat-is-kaptunk-a-jatekbol",
+        "keywords": [
+        "Hírblock"
+        ],
+        "creator": [
+        "info@gamechannel.hu (Maniac)"
+        ],
+        "video_url": null,
+        "description": "A tegnap esti órákban lezajlott PlayStation Showcase online esemény részeként minden, és valóban minden a Ghostwire: Tokyo című videojátékról szólt, mely csapat bár manapság a Microsoft égisze alatt ügyködik, azonban van még egy tartozásuk a Sony felé. Ezt pedig nemsokára törlesztik majd, hiszen a kérdéses eventen #link#htt...",
+        "content": null,
+        "pubDate": "2022-02-04 07:05:17",
+        "image_url": null,
+        "source_id": "gamechannel",
+        "country": [
+        "hungary"
+        ],
+        "category": [
+        "entertainment"
+        ],
+        "language": "hungarian"
+        },
+{
+"title": "Red Bull-kopstuk ziet geen ego bij Verstappen: \"Niemand is groter dan het team\"",
+"link": "https://racingnews365.nl/red-bull-manager-ziet-geen-ego-bij-verstappen-niemand-is-groter-dan-het-team",
+"keywords": null,
+"creator": [
+"Marnik Kok"
+],
+"video_url": null,
+"description": "Jonathan Wheatley, sportief directeur van Red Bull, legt uit hoe de belangrijk het is om een goede band te hebben met Max Verstappen en Sergio Perez. Wat nóg belangrijker is, is dat de twee F1-rijders hun plaats binnen het team kennen en zichzelf niet belangrijker gaan zien dan het geheel.",
+"content": "Jonathan Wheatley heeft als sportief directeur een zeer belangrijke functie binnen Red Bull Racing. Het managen van het team op het circuit en ervoor zorgen dat het team zich aan de sportieve reglementen houdt zijn de belangrijkste onderdelen van zijn baan, maar ook het contact tussen het personeel is van onschatbare waarde. In The Jack Threlfall Show legt Wheatley uit dat hij elke medewerker net zo waardevol vindt binnen het team, van werkzaamheden in de fabriek tot aan de Formule 1-coureurs. Ook Max Verstappen en Sergio Perez zijn 'gewoon' in dienst van Red Bull. \"Ze werken voor het team\", zo trapt het kopstuk af. \"Ik denk dat het belangrijk is om daar geen onderscheid in te maken.\" \"Niemand is groter dan het team. Onze coureurs begrijpen dat en ik heb denk ik nog nooit, misschien één keer, gewerkt met een coureur die een enorm ego had. Niets maakte voor hem uit. Normaal gesproken, als je het tot de Formule 1 geschopt hebt, dan heb je al veel met teams gewerkt. Dan weet je dat het niet alleen om jou draait en dat ieder persoon iets bijdraagt aan het team.\" Band verbeterd sinds de F1 veiliger is geworden Als teammanager werkt Wheatley logischerwijs vaak nauw samen met de coureurs. \"Zeker door alle interacties met de FIA en alle momenten met de stewards. Ik werk samen met de coureur om een perfecte professionele relatie te krijgen.\" Dit is volgens de Red Bull-manager tegenwoordig heel anders, want vroeger was dit bijna niet mogelijk. \"Toen ik in de Formule 1 terecht kwam, kwamen coureurs best vaak te overlijden. Het was heel lastig om een nauwe band met ze te hebben, want dat wilde je soms niet echt. Je hield altijd wel een beetje afstand. Door al het werk van de FIA en de teams met betrekking tot de veiligheid, is dat gelukkig niet meer het geval.\" \"Ik werk heel erg close met de coureurs. Dat moet wel. De pitstops, het rijden... het is allemaal een belangrijk onderdeel. Plus, ik probeer ze veel te helpen als ze nog wat jonger zijn.\" Wheatley geeft hierbij graag advies aan wat minder ervaren coureurs, zoals eerder bij Alexander Albon of bij Pierre Gasly. \"Ik heb veel ervaring met mensen en coureurs en daar wil je het beste uit halen.\"",
+"pubDate": "2022-02-04 07:05:00",
+"image_url": null,
+"source_id": "racingnews365",
+"country": [
+"netherland"
+],
+"category": [
+"sports"
+],
+"language": "dutch"
+},
+        {
+        "title": "Martijn Fischer denkt dat dit zijn laatste jaar als Hazes wordt",
+        "link": "https://www.nu.nl/cultuur-overig/6182028/martijn-fischer-denkt-dat-dit-zijn-laatste-jaar-als-hazes-wordt.html",
+        "keywords": [
+        "Boek & Cultuur",
+        "Media en Cultuur"
+        ],
+        "creator": [
+        "NU.nl"
+        ],
+        "video_url": null,
+        "description": "Martijn Fischer is binnenkort weer te zien als André Hazes in de musical Hij Gelooft in Mij. De acteur denkt dat dit zijn laatste jaar wordt als de Amsterdamse volkszanger, zegt hij vrijdag in het AD.",
+        "content": null,
+        "pubDate": "2022-02-04 07:03:59",
+        "image_url": "https://media.nu.nl/m/npvxpayag8lq_sqr256.jpg/martijn-fischer-denkt-dat-dit-zijn-laatste-jaar-als-hazes-wordt.jpg",
+        "source_id": "nu",
+        "country": [
+        "netherland"
+        ],
+        "category": [
+        "top"
+        ],
+        "language": "dutch"
+        },
+{
+"title": "CLAAS Dominator 68 S Mähdrescher für 32.900 EUR #maschinedestages #gebrauchtmaschine",
+"link": "https://www.agrarheute.com/technik/claas-dominator-68-s-maehdrescher-fuer-32900-eur-589979",
+"keywords": null,
+"creator": null,
+"video_url": null,
+"description": "Unsere Maschine des Tages auf technikboerse.com: CLAAS-Mähdrescher, Modell Dominator 68 S. Das sind die Details.",
+"content": null,
+"pubDate": "2022-02-04 07:03:47",
+"image_url": null,
+"source_id": "agrarheute",
+"country": [
+"germany"
+],
+"category": [
+"top",
+"health"
+],
+"language": "german"
+},
+        {
+        "title": "So sieht eine Junglandwirtin die Zukunft des Pflanzenschutzes #agrarblogger #gespräch",
+        "link": "https://www.agrarheute.com/land-leben/so-sieht-junglandwirtin-zukunft-pflanzenschutzes-589947",
+        "keywords": null,
+        "creator": null,
+        "video_url": null,
+        "description": "Marie Saudhof hat einen Webtalk zum Thema Pflanzenschutz organisiert. Denn: „Die Zeit, um über Alternativen zu reden, ist jetzt.\"",
+        "content": null,
+        "pubDate": "2022-02-04 07:03:47",
+        "image_url": null,
+        "source_id": "agrarheute",
+        "country": [
+        "germany"
+        ],
+        "category": [
+        "top",
+        "health"
+        ],
+        "language": "german"
+        },
+    {
+    "title": "Itt az új Red Hot Chili Peppers dal",
+    "link": "http://hvg.hu/kultura/20220204_Itt_az_uj_Red_Hot_Chili_Peppers_dal#rss",
+    "keywords": null,
+    "creator": [
+    "hvg@hvg.hu"
+    ],
+    "video_url": null,
+    "description": "Az ausztrál bozóttüzeknek emléket állító Black Summer a zenekar következő, Unlimited Love címet viselő sorlemezét harangozza be.",
+    "content": null,
+    "pubDate": "2022-02-04 07:03:00",
+    "image_url": "https://images.hvg.hu/image.aspx?id=410c539f-1777-412a-a484-91e518cb0eb6",
+    "source_id": "hvg",
+    "country": [
+    "hungary"
+    ],
+    "category": [
+    "entertainment"
+    ],
+    "language": "hungarian"
+    }
+
+]
